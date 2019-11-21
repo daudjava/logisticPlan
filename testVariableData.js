a4589c5fd4d72cce553e8a1b1c4495be93e3045f
Andri_Pratama <bargemonitoring.bts-adaro@outlook.com>
2019-11-21 19:07:08 +0700

    little bit change for testing

--- testVariableData.js
+++ testVariableData.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
   var start = today.clone();
   var end = today.clone().add(2, 'day');
   var customTime = today.clone().add(28, 'hour');
-
+  var timelineUTC = '';
+  var timelineLocal = '';
   var items = new vis.DataSet([
     { id: 1, content: 'item 1', start: today.clone().add(8, 'hour') },
     { id: 2, content: 'item 2', start: today.clone().add(16, 'hour') },
@@ -16,21 +17,37 @@ $(document).ready(function() {
   let option1 = {
     editable: true,
     start: start,
-    end: end
+    end: end,
+    onMove: function(item, callback) {
+      //when resize item
+      callback(item); // send back adjusted new item
+      console.log(timelineUTC.itemsData.get());
+    }
   };
 
   let option2 = {
     editable: true,
     start: start,
     end: end,
-    moment: function(date) {
-      return vis.moment(date).utc();
+    onMove: function(item, callback) {
+      //when resize item
+      callback(item); // send back adjusted new item
+      console.log(timelineUTC.itemsData.get());
     }
   };
   // Create a timeline displaying in UTC
-  var timelineUTC = new vis.Timeline(document.getElementById('utc'), items, option2);
-  timelineUTC.addCustomTime(customTime);
+  const el1 = document.getElementById('utc');
 
-  var timelineLocal = new vis.Timeline(document.getElementById('local'), items, option1);
-  timelineLocal.addCustomTime(customTime);
+  const el2 = document.getElementById('local');
+  if (el1 != null) {
+    console.log(el1);
+    console.log('el1');
+    timelineUTC = new vis.Timeline(el1, items, option2);
+    timelineUTC.addCustomTime(customTime);
+  } else {
+    console.log(el2);
+    console.log('el2');
+    timelineLocal = new vis.Timeline(el2, items, option1);
+    timelineLocal.addCustomTime(customTime);
+  }
 });
